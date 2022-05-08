f476c8796ef0c3e20d22affbf76ea9175792e598
Mikasa <3127736010@qq.com>
2022-05-08 20:05:02 +0800

    sql-schema

---
+++ MyBookServer/src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+insert into book ( name, author, cover, price, category_id, book_info_id) values ('第一行代码','郭霖','/photoes/1.jpg','79.00',12,24);
+insert into book ( name, author, cover, price, category_id, book_info_id) values ('深入分析Java Web 技术内幕','许令波','/photoes/2.jpg','79.00',12,24);
+insert into book ( name, author, cover, price, category_id, book_info_id) values ('Spring In Action','tim','/photoes/3.jpg','89.00',12,34);
+insert into book ( name, author, cover, price, category_id, book_info_id) values ('Java核心技术 卷I','[美]凯·S.霍斯特曼','/photoes/4.jpg','129.00',12,44);
